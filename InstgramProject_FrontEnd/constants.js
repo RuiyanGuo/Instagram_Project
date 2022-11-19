3999275edce99a2c4a3ce635d4a39fc417258515
Ruiyan Guo <100314093+RuiyanGuo@users.noreply.github.com>
2022-11-19 11:26:33 -0500

    Add FrontEnd

---
+++ InstgramProject_FrontEnd/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://instgram-project.nn.r.appspot.com';//mine
+//export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
+//export const BASE_URL = 'http://34.130.69.113:8080';
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+   };
